c6ec97ccd1c4ff17f3597977cca112ff99268834
Joe Degiovanni <jdegiovanni@lifetouch.com>
2020-01-01 23:47:41 -0600

    spring boot initializer

---
+++ src/main/groovy/com/mnstreetmarket/membershiptracker/MembershipTrackerApplication.groovy
@@ -0,0 +1,13 @@
+package com.mnstreetmarket.membershiptracker
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MembershipTrackerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MembershipTrackerApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/mnstreetmarket/membershiptracker/MembershipTrackerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.mnstreetmarket.membershiptracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MembershipTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
