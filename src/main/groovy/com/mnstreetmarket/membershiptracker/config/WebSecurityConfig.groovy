9c97e5cd58c308b8a871341ffb75e30abee73807
Joe Degiovanni <jdegiovanni@lifetouch.com>
2020-01-04 21:12:08 -0600

    modify member entity

--- src/main/groovy/com/mnstreetmarket/membershiptracker/config/WebSecurityConfig.groovy
+++ src/main/groovy/com/mnstreetmarket/membershiptracker/config/WebSecurityConfig.groovy
@@ -1,5 +1,6 @@
 package com.mnstreetmarket.membershiptracker.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -8,14 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value('${spring.h2.console.enabled}')
+    boolean h2ConsoleEnabled
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers('/', '/join', '/h2-console/**').permitAll()
                 .anyRequest().authenticated().and().formLogin()
 
-        http.csrf().disable()
-        http.headers().frameOptions().disable()
+        if (h2ConsoleEnabled) {
+            http.csrf().disable()
+            http.headers().frameOptions().disable()
+        }
     }
 }
