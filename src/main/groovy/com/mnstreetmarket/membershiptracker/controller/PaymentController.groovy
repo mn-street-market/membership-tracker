b6d3ccd968930525aba64f9985d0da0ff5b43a61
jdegiovanni <jdegiovanni@workfusion.com>
2022-07-16 21:54:18 +0100

    add test Stripe integration

--- src/main/groovy/com/mnstreetmarket/membershiptracker/controller/PaymentController.groovy
+++ src/main/groovy/com/mnstreetmarket/membershiptracker/controller/PaymentController.groovy
@@ -1,16 +1,20 @@
 package com.mnstreetmarket.membershiptracker.controller
 
-
-import com.mnstreetmarket.membershiptracker.config.StripeConfig
+import com.mnstreetmarket.membershiptracker.entity.MemberEntity
 import com.mnstreetmarket.membershiptracker.enums.ApplicationView
+import com.mnstreetmarket.membershiptracker.repository.MemberRepository
 import com.stripe.model.checkout.Session
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 import javax.annotation.security.RolesAllowed
+import java.sql.Timestamp
+import java.time.Instant
 
 @Controller
 @RequestMapping('/pay')
@@ -19,25 +23,22 @@ import javax.annotation.security.RolesAllowed
 class PaymentController {
 
     @Autowired
-    StripeConfig stripeConfig
-    
+    MemberRepository memberRepository
+
     @GetMapping
     String get(@RequestParam(required = false, name = "session_id") String checkoutSessionId, Model model) {
-        
+
         Session session = Session.retrieve(checkoutSessionId);
-        
-        if(session.paymentStatus == 'paid') {
+
+        if (session.paymentStatus == 'paid') {
+            MemberEntity member = memberRepository.findByEmail(session.customerEmail)
+                    .orElseThrow { new IllegalArgumentException("Unable to find customer with email '${session.customerEmail}'") }
+            member.setMembershipPaidDate(new Timestamp(Instant.now().toEpochMilli()))
             model.addAttribute("alert", "it worked! $session.customerEmail has paid!")
         } else {
-            model.addAttribute('alert', 'failure!')
+            model.addAttribute('alert', 'payment not received')
         }
-        
-        return ApplicationView.PAYMENT
-    }
-    
-    @PostMapping
-    String post(@RequestBody Session checkoutSession) {
-        log.info("payment accepted $checkoutSession")
+
         return ApplicationView.PAYMENT
     }
 
