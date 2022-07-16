f2a2e19c3d15d2636a83778507130d54e51831b4
jdegiovanni <jdegiovanni@workfusion.com>
2022-07-16 21:39:18 +0100

    add test Stripe integration

--- src/main/groovy/com/mnstreetmarket/membershiptracker/controller/PaymentController.groovy
+++ src/main/groovy/com/mnstreetmarket/membershiptracker/controller/PaymentController.groovy
@@ -1,6 +1,6 @@
 package com.mnstreetmarket.membershiptracker.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
+
 import com.mnstreetmarket.membershiptracker.config.StripeConfig
 import com.mnstreetmarket.membershiptracker.enums.ApplicationView
 import com.stripe.model.checkout.Session
@@ -8,11 +8,7 @@ import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 
 import javax.annotation.security.RolesAllowed
 
@@ -25,9 +21,6 @@ class PaymentController {
     @Autowired
     StripeConfig stripeConfig
     
-    @Autowired
-    ObjectMapper objectMapper
-
     @GetMapping
     String get(@RequestParam(required = false, name = "session_id") String checkoutSessionId, Model model) {
         
